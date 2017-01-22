93cf560979603df992f694eeeb243abaef061c58
Monish Gupta <monish.gupta1@gmail.com>
2017-01-22 20:00:12 +0530

    Added code for react spinner

--- src/ReactSpinner.js
+++ src/ReactSpinner.js
@@ -1,8 +1,32 @@
 var React = require('react');
 
 var ReactSpinner = React.createClass({
-	render () {
-		return <div className="mg-react-spinner">React Spinner</div>;
+	getDefaultProps: function() {
+    return {
+      size: 20
+    };
+  },
+	render: function() {
+		let spinnerClass = 'mg-react-spinner__main' + " " + 'l-center';
+    let size = this.props.size;
+    let margin = -1*(size/2);
+    return (
+    <div
+      className={(this.props.state==="hide"?"hide ":" ") + 'mg-react-spinner'}
+      style={{
+        'minHeight': size+'px'
+      }}>
+      <div
+        className={spinnerClass}
+        style={{
+          'width': size+'px',
+          'height': size+'px',
+          'marginTop': margin,
+          'marginLeft': margin,
+          'borderWidth': 2*size/15
+        }}
+      ></div>
+    </div>)
 	}
 });
 
